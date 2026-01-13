1824b541efdcbec07d00b3b69ce0d9c1fc239373
Douglas Viliano <dougvil@msn.com>
2026-01-13 23:34:22 +0000

    Add home page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Oi, meu amor! Obrigado por ser minha parceira de vida! Amo você e a nossa
+      família perfeita!
+    </h1>
+  );
+}
+
+export default Home;
