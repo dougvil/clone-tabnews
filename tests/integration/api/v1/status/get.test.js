16c382dfe8d32b229d431646d672f6ef45271e11
Douglas <dougvil@msn.com>
2026-01-22 14:18:20 -0300

    Cria endpoint status

---
+++ tests/integration/api/v1/status/get.test.js
@@ -0,0 +1,6 @@
+test('GET /api/v1/status', async () => {
+  const res = await fetch('http://localhost:3000/api/v1/status');
+  expect(res.status).toBe(200);
+  const data = await res.json();
+  expect(data).toEqual({ status: 'OK' });
+});
