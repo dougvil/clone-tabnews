6f7e20379abdf0a701af07031956fe355fa67c7b
Douglas <dougvil@msn.com>
2026-01-27 22:10:44 -0300

    Add jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,7 @@
-import database from '../../../../infra/database';
+import database from 'infra/database';
 
 async function status(req, res) {
   const result = await database.query('SELECT 1+1 as result;');
-  console.log(result);
   res.status(200).json({ status: 'OK' });
 }
 
