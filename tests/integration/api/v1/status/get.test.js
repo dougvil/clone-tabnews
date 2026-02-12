e21ba0e5c11924af8bac5fad81752d60e1d52211
Douglas <dougvil@msn.com>
2026-02-11 23:06:36 -0300

    Add ssl to database connection config

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,4 +10,4 @@ test('GET /api/v1/status', async () => {
   expect(responseBody.dependencies.database.version).toEqual('16.0');
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.current_connections).toEqual(1);
-});
+}, 10000);
