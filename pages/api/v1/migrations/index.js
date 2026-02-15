fe762ef3e983cf82978e439ced34dbe5defae26e
Douglas <dougvil@msn.com>
2026-02-14 22:33:23 -0300

    fix: avoid other methods than GET and POST

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -4,32 +4,46 @@ import { join } from 'path';
 import database from 'infra/database';
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient,
-    dir: join(process.cwd(), 'infra', 'migrations'),
-    dryRun: true,
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pgmigrations',
-  };
-
-  if (req.method === 'GET') {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return res.status(200).json(pendingMigrations);
+  const allowedMethods = ['GET', 'POST'];
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).end();
   }
 
-  if (req.method === 'POST') {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    if (migratedMigrations.length === 0) {
-      return res.status(200).json([]);
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient,
+      dir: join(process.cwd(), 'infra', 'migrations'),
+      dryRun: true,
+      direction: 'up',
+      verbose: true,
+      migrationsTable: 'pgmigrations',
+    };
+
+    if (req.method === 'GET') {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      return res.status(200).json(pendingMigrations);
     }
-    return res.status(201).json(migratedMigrations);
+
+    if (req.method === 'POST') {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      if (migratedMigrations.length === 0) {
+        return res.status(200).json([]);
+      }
+      return res.status(201).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: 'An error occurred while running migrations.' });
+  } finally {
+    console.log('Closing database connection');
+    await dbClient.end();
   }
-  return res.status(405).end();
 }
