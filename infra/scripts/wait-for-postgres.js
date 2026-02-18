dcd34c65fcc5ca0db0b175d54a6ca06369106731
Douglas <dougvil@msn.com>
2026-02-18 20:47:54 -0300

    Add wait-for-postgres script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,18 @@
+const { exec } = require('node:child_process');
+
+function waitForPostgres() {
+  exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
+
+  function handleReturn(error, stdout, stderr) {
+    if (error) {
+      console.log('PostgreSQL is not ready yet. Retrying in 2 seconds...');
+      setTimeout(waitForPostgres, 2000);
+    } else {
+      console.log('🟢 PostgreSQL is ready!');
+      process.exit(0);
+    }
+  }
+}
+
+console.log('🔴 Waiting for PostgreSQL to be ready...');
+waitForPostgres();
