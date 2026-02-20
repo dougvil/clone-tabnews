74d990d9ffa1a6e1fee0c352e5ae99f46f9b86b6
Douglas Viliano <dougvil@msn.com>
2026-02-19 23:22:30 -0300

    Adds "lint:eslint:check" + `eslint` job inside 'Linting' GitHub actions (#15)
    * Adds eslint configuration
    
    * Add eslint workflow
    
    * Fix eslint linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -3,7 +3,7 @@ const { exec } = require('node:child_process');
 function waitForPostgres() {
   exec('docker exec postgres-dev pg_isready --host localhost', handleReturn);
 
-  function handleReturn(error, stdout, stderr) {
+  function handleReturn(error) {
     if (error) {
       console.log('PostgreSQL is not ready yet. Retrying in 2 seconds...');
       setTimeout(waitForPostgres, 2000);
