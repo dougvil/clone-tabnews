f724c1b984d4d166766841411a8fab6e9702bbc7
Douglas <dougvil@msn.com>
2026-02-25 11:05:05 -0300

    test: improve test descriptions

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -11,11 +11,25 @@ beforeAll(async () => {
   return clearDatabase();
 });
 
-test('GET /api/v1/migrations', async () => {
-  const res = await fetch('http://localhost:3000/api/v1/migrations');
-  expect(res.status).toBe(200);
+describe('GET /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    describe('when running pending migrations', () => {
+      test('for the first time', async () => {
+        const res = await fetch('http://localhost:3000/api/v1/migrations');
+        expect(res.status).toBe(200);
 
-  const responseBody = await res.json();
-  expect(Array.isArray(responseBody)).toEqual(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-}, 10000);
+        const responseBody = await res.json();
+        expect(Array.isArray(responseBody)).toEqual(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      }, 10000);
+      test('for the second time', async () => {
+        const res = await fetch('http://localhost:3000/api/v1/migrations');
+        expect(res.status).toBe(200);
+
+        const responseBody = await res.json();
+        expect(Array.isArray(responseBody)).toEqual(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+      });
+    });
+  });
+});
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,16 +4,20 @@ beforeAll(async () => {
   await orquestrator.waitForAllServices();
 });
 
-test('GET /api/v1/status', async () => {
-  const res = await fetch('http://localhost:3000/api/v1/status');
-  expect(res.status).toBe(200);
+describe('GET /api/v1/status', () => {
+  describe('Anonymous user', () => {
+    test('Retrieving current system status', async () => {
+      const res = await fetch('http://localhost:3000/api/v1/status');
+      expect(res.status).toBe(200);
 
-  const responseBody = await res.json();
+      const responseBody = await res.json();
 
-  const parsedDate = new Date(responseBody.updated_at).toISOString();
-  expect(parsedDate).toBe(responseBody.updated_at); // Verifica se é uma data válida
+      const parsedDate = new Date(responseBody.updated_at).toISOString();
+      expect(parsedDate).toBe(responseBody.updated_at);
 
-  expect(responseBody.dependencies.database.version).toEqual('16.0');
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.current_connections).toEqual(1);
-}, 10000);
+      expect(responseBody.dependencies.database.version).toEqual('16.0');
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.current_connections).toEqual(1);
+    });
+  });
+});
