8126830c8b5057003f48cfdc674784e7550ad458
Douglas <dougvil@msn.com>
2026-02-28 21:39:09 -0300

    feat: add custom errors

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import database from 'infra/database';
+import { InternalServerError } from 'infra/errors';
 
 type DatabaseStatus = {
   version: string;
@@ -14,34 +15,44 @@ type StatusResponse = {
   };
 };
 
+type ErrorResponse = {
+  message: string;
+};
+
 export default async function status(
   req: NextApiRequest,
-  res: NextApiResponse<StatusResponse>,
+  res: NextApiResponse<StatusResponse | ErrorResponse>,
 ) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  const databaseVersion = await database.query({
-    text: 'SHOW server_version;',
-  });
+    const databaseVersion = await database.query({
+      text: 'SHOW server_version;',
+    });
 
-  const maxConnections = await database.query({
-    text: 'SHOW max_connections;',
-  });
+    const maxConnections = await database.query({
+      text: 'SHOW max_connections;',
+    });
 
-  const databaseName = process.env.POSTGRES_DB;
-  const currentConnections = await database.query({
-    text: `SELECT COUNT(*)::int FROM pg_stat_activity where datname = $1;`,
-    values: [databaseName],
-  });
+    const databaseName = process.env.POSTGRES_DB;
+    const currentConnections = await database.query({
+      text: `SELECT COUNT(*)::int FROM pg_stat_activity where datname = $1;`,
+      values: [databaseName],
+    });
 
-  res.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: databaseVersion.rows[0].server_version,
-        max_connections: parseInt(maxConnections.rows[0].max_connections, 10),
-        current_connections: currentConnections.rows[0].count,
+    res.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersion.rows[0].server_version,
+          max_connections: parseInt(maxConnections.rows[0].max_connections, 10),
+          current_connections: currentConnections.rows[0].count,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({ cause: error });
+    console.error(publicErrorObject);
+    res.status(500).json(publicErrorObject);
+  }
 }
