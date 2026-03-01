d1812aa85055da6bb88200ec6482dc1fe7709d08
Douglas <dougvil@msn.com>
2026-03-01 17:54:43 -0300

    fix: update jest config for ESM dependencies

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,21 @@ const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({ dir: '.' });
 
-const jestConfig = createJestConfig({
+const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/src'],
   testTimeout: 30000,
-});
+};
+
+const jestConfig = async () => {
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  return {
+    ...nextJestConfig,
+    transformIgnorePatterns: ['/node_modules/(?!(node-pg-migrate|uuid))'],
+    moduleNameMapper: {
+      '^file://(/.*)$': '$1',
+      ...nextJestConfig.moduleNameMapper,
+    },
+  };
+};
 
 module.exports = jestConfig;
